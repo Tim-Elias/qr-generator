62c09a1a4a406e125a2990b0657308ee4e3ee37b
Ilia Timofeev <ilya48007@gmail.com>
2024-04-23 12:51:03 +0700

    qr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/:data')
-  getHello(@Param('data') data: string): string {
+  @Get()
+  getHello(@Query('data') data: string): string {
     return this.appService.getQR(data);
   }
 }
